04d1d0646afdcde1554bdb1aa99695b3b5d52144
franciskabacurin <153901415+franciskabacurin@users.noreply.github.com>
2024-03-22 09:19:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/programiranje")
+def programiranje():
+    return render_template("programiranje.html")
+
+@app.route("/python")
+def python():
+    return render_template("python.html")
+
+if __name__ == "__main__":
+    app.run()
